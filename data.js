11e7268c6b67b4fc4c54a20be12ef841c7a437d5
diegoocezar <diego.bernardino@amedigital.com>
2020-08-24 23:41:02 -0300

    create data file and add images

---
+++ data.js
@@ -0,0 +1,57 @@
+export default [
+  {
+    key: '1',
+    model: 'Nano 9',
+    store: 'Reebok',
+    logo: require('./src/assets/img/reebok-logo.png'),
+    image: require('./src/assets/img/nano9-blue.png'),
+    title: 'O certo para o seu wod',
+    color: '#9dcdfa',
+    description:
+      'Encare novos desafios com o Nano 9 nos pés. O cabedal Stretch Flexweave® proporciona um ajuste mais seguro e a estrutura central é ideal para dias de levantamento de peso.',
+  },
+  {
+    key: '2',
+    model: 'Metcon 5',
+    store: 'Nike',
+    logo: require('./src/assets/img/nike-logo.png'),
+    image: require('./src/assets/img/metcon5.png'),
+    title: 'Nosso Metcon para elas',
+    color: '#E7C4BD',
+    description:
+      'Fabricado com material resistente e durável, o Tênis Nike Metcon 5 Feminino é a escolha perfeita para pegar pesado nos treinos diários.',
+  },
+  {
+    key: '3',
+    model: 'Metcon 6',
+    store: 'Nike',
+    logo: require('./src/assets/img/nike-logo.png'),
+    image: require('./src/assets/img/metcon6-black.png'),
+    title: 'Nosso Metcon mais estável',
+    color: '#5B585F',
+    description:
+      'O Nike Metcon 5 é o Metcon mais ajustado já fabricado. Isso significa estabilidade específica para levantamento pesado, tração feita para velocidade e durabilidade onde você precisar.',
+  },
+  {
+    key: '4',
+    model: 'Nano 9',
+    store: 'Reebok',
+    logo: require('./src/assets/img/reebok-logo.png'),
+    image: require('./src/assets/img/nano9-orange.png'),
+    title: 'O certo para o seu wod',
+    color: '#B97145',
+    description:
+      'Encare novos desafios com o Nano 9 nos pés. O cabedal Stretch Flexweave® proporciona um ajuste mais seguro e a estrutura central é ideal para dias de levantamento de peso.',
+  },
+  {
+    key: '5',
+    model: 'Nano 9',
+    store: 'Reebok',
+    logo: require('./src/assets/img/reebok-logo.png'),
+    image: require('./src/assets/img/nano9-purple.png'),
+    title: 'O certo para o seu wod',
+    color: '#945FB7',
+    description:
+      'Encare novos desafios com o Nano 9 nos pés. O cabedal Stretch Flexweave® proporciona um ajuste mais seguro e a estrutura central é ideal para dias de levantamento de peso.',
+  },
+];
